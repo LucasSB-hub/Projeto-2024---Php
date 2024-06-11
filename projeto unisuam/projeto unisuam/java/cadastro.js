dead2168fe782fbf12ab24cd33353c7855723fcc
thomaz-miguel <132624142+thomaz-miguel@users.noreply.github.com>
2024-06-10 21:10:19 -0300

    atualizado, faltando linkar a tela master

--- projeto unisuam/projeto unisuam/java/cadastro.js
+++ projeto unisuam/projeto unisuam/java/cadastro.js
@@ -108,12 +108,3 @@ window.onload = function () {
         window.location.href = "login.html";
     });
 }
-
-        // JavaScript adicional do Miguel
-        let menu = document.querySelector('#menu-icon');
-        let navlist = document.querySelector('.navlist');
-
-        menu.onclick = () => {
-            menu.classList.toggle('bx-x');
-            navlist.classList.toggle('open')
-}
