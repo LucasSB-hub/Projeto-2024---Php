6fe2ca4b241a8e9173a19f5c8112ba362e1f20af
ExchangedCorgi <134800669+NeoExchangedCorgi@users.noreply.github.com>
2024-06-19 15:58:46 -0300

    linkado em consulta.php

--- projeto unisuam/java/tela.js
+++ projeto unisuam/java/tela.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('consulta-button').addEventListener('click', function() {
-        window.location.href = 'consulta.html';
+        window.location.href = 'consulta.php';
     });
 
     document.getElementById('log-button').addEventListener('click', function() {
